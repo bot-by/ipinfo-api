ee4634639eb95bd1086f14a2046da34845e319fb
Witalij Berdinskich <radio_rogal@keemail.me>
2023-04-16 11:40:22 +0300

    Update package, javadoc and dependencies

--- src/main/java/uk/bot_by/ipinfo_api/TokenInterceptor.java
+++ src/main/java/uk/bot_by/ipinfo_api/TokenInterceptor.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.bot_by.ipinfo;
+package uk.bot_by.ipinfo_api;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
